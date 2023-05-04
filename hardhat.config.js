ba6f6d1c5fd1c9bbb75a2d5463094a82cc7cb467
satnam_singh <satnamm143@gmail.com>
2023-05-04 23:08:04 +0530

    uniswapV3-Swap

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.6",
+        settings: {
+          evmVersion: "istanbul",
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_KEY",
+      },
+    },
+  },
+};
